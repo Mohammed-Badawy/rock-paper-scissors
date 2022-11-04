f9219c87f86e320a47be427e8103e6366604f421
Mohammed Badawy <115544356+Mohammed-Badawy@users.noreply.github.com>
2022-11-04 08:21:06 +0200

    Adjust playRound function
    Added new function called roundWinner that returns a string
    to announce the winner of the round. playRound function will
    return an integer to decide the winner of the round.
    player wins = 1; --- tie = 0; --- computer wins = -1;

--- js/app.js
+++ js/app.js
@@ -12,32 +12,52 @@ function getComputerChoice(){
 
 /* create fuction to play one round against the computer */
 function playRound(playerSelection, computerSelection){
+    // player wins  = 1;  tie = 0; computer wins = -1;
+    let playerWin = null; 
 
     // retun lowercase choice
     playerSelection = playerSelection.toLowerCase();
 
     // paper <==> rock  ===> paper (player won)
     if(playerSelection === "paper" && computerSelection === "rock"){
-        return `You Won this round! ${playerSelection} beats ${computerSelection}`;
+        playerWin = 1;
     }
     // rock <==> scissors ===> rock (player won)
     else if(playerSelection === "rock" && computerSelection === "scissors"){
-        return `You Won this round! ${playerSelection} beats ${computerSelection}`;
+       playerWin = 1;
     }
     // scissors <==> paper scissors (player won)
     else if(playerSelection === "scissors" && computerSelection === "paper"){
-        return `You Won this round! ${playerSelection} beats ${computerSelection}`;
+        playerWin = 1;
     }
     // playerSelection === computerSelection ===> (tie)
     else if(playerSelection === computerSelection){
-        return `There is a tie`;
+        playerWin = 0;
     }
     // else (computer won)
     else {
-        return `You lose this round! ${computerSelection} beats ${playerSelection}`
+        playerWin = -1;
     }
 
+    console.log(roundWinner(playerWin, playerSelection, computerSelection));
+    return playerWin;
 
+}
+
+// create function returns string to announce the winner of the round
+function roundWinner(winner, player, computer){
+    switch(winner){
+        case 1:
+            return `You Won this round! ${player} beats ${computer}`;
+            break;
+        
+        case -1:
+            return `You lose this round! ${computer} beats ${player}`;
+            break;
+        
+        default:
+            return `There is a tie`;
+    }
 }
 
 const playerChoice = "PAper";

71d60d2510030804f5dee0cdeea17e745b5afe3e
Mohammed Badawy <115544356+Mohammed-Badawy@users.noreply.github.com>
2022-11-04 07:40:25 +0200

    Add function to play one round and return a winner
    Added function that takes two values and decide the winner.
    It returns a string to announce the winner of the round.

--- js/app.js
+++ js/app.js
@@ -10,4 +10,37 @@ function getComputerChoice(){
     return choice;
 }
 
-console.log(getComputerChoice());
+/* create fuction to play one round against the computer */
+function playRound(playerSelection, computerSelection){
+
+    // retun lowercase choice
+    playerSelection = playerSelection.toLowerCase();
+
+    // paper <==> rock  ===> paper (player won)
+    if(playerSelection === "paper" && computerSelection === "rock"){
+        return `You Won this round! ${playerSelection} beats ${computerSelection}`;
+    }
+    // rock <==> scissors ===> rock (player won)
+    else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return `You Won this round! ${playerSelection} beats ${computerSelection}`;
+    }
+    // scissors <==> paper scissors (player won)
+    else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return `You Won this round! ${playerSelection} beats ${computerSelection}`;
+    }
+    // playerSelection === computerSelection ===> (tie)
+    else if(playerSelection === computerSelection){
+        return `There is a tie`;
+    }
+    // else (computer won)
+    else {
+        return `You lose this round! ${computerSelection} beats ${playerSelection}`
+    }
+
+
+}
+
+const playerChoice = "PAper";
+const computerChoice = getComputerChoice();
+
+console.log(playRound(playerChoice, computerChoice));
